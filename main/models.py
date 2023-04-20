ce194f6a03415d254583ff57cf1d750d0ef48ccc
Doublesaurus <rklepacs@gmail.com>
2023-04-20 01:56:56 -0400

    Added class Country

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    symbol = models.CharField(max_length=64)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+
