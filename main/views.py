cededffeea701141375b05ce1714a1ba5c5e48dc
Doublesaurus <rklepacs@gmail.com>
2023-04-20 01:55:45 -0400

    added home view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Country
 
 # Create your views here.
+def home(request):
+    countries = Country.objects.all()
+    rest_countries_api = "https://restcountries.com/v3.1/all"
+    return render(request, 'index.html', {"countries": countries})
